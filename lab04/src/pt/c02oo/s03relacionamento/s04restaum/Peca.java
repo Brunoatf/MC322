124429f4b0f30deebf1e0c9f07b00fb30245bca4
Bruno Amaral Teixeira de Freitas <brunoatf257@gmail.com>
2022-04-13 16:12:40 -0300

    ajustes

--- lab04/src/pt/c02oo/s03relacionamento/s04restaum/Peca.java
+++ lab04/src/pt/c02oo/s03relacionamento/s04restaum/Peca.java
@@ -1,17 +1,21 @@
 package pt.c02oo.s03relacionamento.s04restaum;
 
-public class peca {
+public class Peca {
 
-	char tipo; 
+	private char tipo; 
 	//casas vazias do tabuleiro foram consideradas como pecas de tipo '-'
 	//casas preenchidas com pecas padrao foram consideradas como pecas de tipo 'P'
 	//posicoes de fora do tabuleiro foram consideradas como pecas de tipo ' '
 	
-	peca (char tipo) {
+	public Peca (char tipo) {
 		this.tipo = tipo;
 	}
 	
-	void atualizarEstado() { //atualiza o estado da peca, que pode variar entre uma peca padrao e um espaco vazio
+	public char getTipo() {
+		return tipo;
+	}
+	
+	public void atualizarEstado() { //atualiza o estado da peca, que pode variar entre uma peca padrao e um espaco vazio
 		if (tipo == 'P') {
 			tipo = '-';
 		}
--- lab04/src/pt/c02oo/s03relacionamento/s04restaum/Tabuleiro.java
+++ lab04/src/pt/c02oo/s03relacionamento/s04restaum/Tabuleiro.java
@@ -2,10 +2,10 @@ package pt.c02oo.s03relacionamento.s04restaum;
 
 public class Tabuleiro {
 
-	peca[][] pecas = new peca[7][7]; //o tabuleiro sera armazenado como uma matriz de objetos peca, que podem representar pecas, espacos vazios e casas de fora do tabuleiro
-	String ultimoMovimento = null; //guarda a string do ultimo movimento executado sobre o tabuleiro
+	private Peca[][] pecas = new Peca[7][7]; //o tabuleiro sera armazenado como uma matriz de objetos peca, que podem representar pecas, espacos vazios e casas de fora do tabuleiro
+	private String ultimoMovimento = null; //guarda a string do ultimo movimento executado sobre o tabuleiro
 	
-	Tabuleiro () {
+	public Tabuleiro () {
 		char[][] referenciaCasas = { //usaremos essa matriz de chars para construir a matriz de objetos peca 
 				{' ', ' ', 'P', 'P', 'P', ' ', ' '},
 				{' ', ' ', 'P', 'P', 'P', ' ', ' '},
@@ -18,33 +18,33 @@ public class Tabuleiro {
 		for (int i=0; i<7; i++) {
 			for (int j=0; j<7; j++) {
 				if (referenciaCasas[i][j] == 'P') {
-					pecas[i][j] = new peca('P');
+					pecas[i][j] = new Peca('P');
 				}
 				else if (referenciaCasas[i][j] == '-') {
-					pecas[i][j] = new peca('-');
+					pecas[i][j] = new Peca('-');
 				}
 				else {
-					pecas[i][j] = new peca(' ');
+					pecas[i][j] = new Peca(' ');
 				}
 			}
 		}
 	}
 	
-	String tituloUltimoMovimento () {
+	public String tituloUltimoMovimento () {
 		return "source: "+ultimoMovimento.substring(0, 2)+"; target:"+ultimoMovimento.substring(3);
 	}
 
-	char[][] estadoAtualTabuleiro() {
+	public char[][] estadoAtualTabuleiro() {
 		char[][] board = new char[7][7];
 		for (int i=0; i<7; i++) {
 			for (int j=0; j<7; j++) {
-				board[i][j] = pecas[i][j].tipo;
+				board[i][j] = pecas[i][j].getTipo();
 			}
 		}
 		return board;
 	}
 	
-	int traduzirCoordenadas(char caractere) {
+	private int traduzirCoordenadas(char caractere) {
 		return switch (caractere) {
 			case 'a' -> 0;
 			case 'b' -> 1;
@@ -59,14 +59,14 @@ public class Tabuleiro {
 
 	private boolean movimentoPossivel(int linhaTarget, int colunaTarget, int linhaSource, int colunaSource) {
 		if (linhaSource > 0 && linhaSource < 7 && colunaSource > 0 && colunaSource < 7 && linhaTarget > 0 && linhaTarget < 7 && colunaTarget > 0 && colunaTarget < 7) {
-			if (pecas[linhaTarget][colunaTarget].tipo == '-' && pecas[linhaSource][colunaSource].tipo == 'P' ) {
+			if (pecas[linhaTarget][colunaTarget].getTipo() == '-' && pecas[linhaSource][colunaSource].getTipo() == 'P' ) {
 				if (linhaSource == linhaTarget) {
-					if (Math.abs(colunaTarget - colunaSource) == 2 && pecas[linhaTarget][(colunaTarget + colunaSource) / 2].tipo == 'P') {
+					if (Math.abs(colunaTarget - colunaSource) == 2 && pecas[linhaTarget][(colunaTarget + colunaSource) / 2].getTipo() == 'P') {
 						return true;
 					}
 				}
 				else if (colunaSource == colunaTarget) {
-					if (Math.abs(linhaTarget - linhaSource) == 2 && pecas[(linhaTarget + linhaSource) / 2][colunaTarget].tipo == 'P') {
+					if (Math.abs(linhaTarget - linhaSource) == 2 && pecas[(linhaTarget + linhaSource) / 2][colunaTarget].getTipo() == 'P') {
 						return true;
 					}	
 				}
@@ -75,7 +75,7 @@ public class Tabuleiro {
 		return false;
 	}
 	
-	void executarMovimento (String posicoes) {
+	public void executarMovimento (String posicoes) {
 
 		int linhaTarget = Integer.parseInt(posicoes.substring(4, 5)) - 1;
 		int linhaSource = Integer.parseInt(posicoes.substring(1, 2)) - 1;
